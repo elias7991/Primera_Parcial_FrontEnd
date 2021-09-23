99f636ae1d25047f634dd454fa4522c4198ca055
elias <eliascaceres124@fpuna.edu.py>
2021-09-22 20:00:51 -0400

    modulo material agregado

--- primerParcial/src/app/app.component.ts
+++ primerParcial/src/app/app.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+/*  animaciones de angular */
+import {
+
+} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  
+  animations: [
+    //triggers de animaciones 
+  ],
+  
 })
 export class AppComponent {
   title = 'primerParcial';
--- primerParcial/src/app/app.module.ts
+++ primerParcial/src/app/app.module.ts
@@ -1,3 +1,5 @@
+/* modulo de animaciones  */
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
@@ -6,16 +8,38 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CategoriaComponent } from './categoria/categoria.component';
 import { ServicecategoriaService } from './service/servicecategoria.service';
+import { LoginComponent } from './login/login.component';
+/* importacion de modulos de material para utilziarlos en el HTML */
+
+/* Modulo tarjetas */
+import { MatCardModule } from '@angular/material/card'
+/* Modulo de formulario */
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+import { MatSliderModule } from '@angular/material/slider';
+
+/*
+  Tener en cuenta que cada vez que importamos un modulo 
+  de material debemos de agregarlo al "imports" de @NgModule
+*/
 
 @NgModule({
   declarations: [
     AppComponent,
-    CategoriaComponent
+    CategoriaComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    /* módulo de animaciones */
+    BrowserAnimationsModule,
+    /*Modulo para tarjeta*/
+    MatCardModule,
+    /* modulo de formulario */
+    MatFormFieldModule,
+    MatSliderModule,
   ],
   providers: [ServicecategoriaService],
   bootstrap: [AppComponent]
